8dafe07124842bc3714a20b8f29b4e6c76d20307
Gurveer Madurai <maduraigurveer@gmail.com>
2023-09-19 11:13:05 -0700

    left sidebar button padding debugged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import LeftSidebar from "@/components/LeftSidebar";
 
 const page = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center bg-black">
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* Left sidebar/ navigation */}
-       <LeftSidebar />
-        <main></main>
+        <LeftSidebar />
+        <main className="ml-[275px] p-6 mx-2 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+          <h1 className="text-2xl font-bold">Home</h1>
+        </main>
         <section></section>
       </div>
     </div>
--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -38,35 +38,37 @@ const NAVIGATION_ITEMS = [
 ];
 const LeftSidebar = () => {
   return (
-    <section className="fixed w-[275] items-stretch flex flex-col h-screen">
-    <div className="w-full mt-4 flex flex-col items-stretch  h-full space-y-4">
-      {NAVIGATION_ITEMS.map((item) => (
-        <Link
-          className="hover:bg-white/10 text-2xl transition duration-200 rounded-3xl py-2 px-6 flex items-center justify-start w-fit space-x-2"
-          href={`/${item.title.toLowerCase()}`}
-          key={item.title}
-        >
-          <div>
-            <item.icon />
+    <section className="fixed w-[275] items-stretch flex flex-col h-screen px-4">
+      <div className="mt-4 flex flex-col items-stretch  h-full space-y-4">
+        {NAVIGATION_ITEMS.map((item) => (
+          <Link
+            className="hover:bg-white/10 text-2xl transition duration-200 rounded-3xl py-2 px-6 flex items-center justify-start w-fit space-x-2"
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+          >
+            <div>
+              <item.icon />
+            </div>
+            {item.title !== "Twitter" && <div>{item.title}</div>}
+          </Link>
+        ))}
+        <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+          Tweet
+        </button>
+      </div>
+      <button className="rounded-full flex items-center space-x-2 bg-transparent p-4 text-center hover:bg-white/10 transition duration-200 w-full justify-between">        <div className="flex items-center space-x-2">
+          <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+          <div className="text-left text-sm">
+            <div className="font-semibold">Club of coders</div>
+            <div>@codeOfCoders.com</div>
           </div>
-          <div>{item.title !== "Twitter" && <div>{item.title}</div>}</div>
-        </Link>
-      ))}
-      <button className="w-full rounded-3xl m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition ">
-        Tweet
+        </div>
+        <div>
+          <BsThreeDots />
+        </div>
       </button>
-    </div>
-    <button className="w-full flex items-center space-x-2 rounded-3xl m-4 p-4 text-2xl text-center hover:bg-white/10 transition duration-200 justify-between ">
-      <div className="flex items-center space-x-2">
-      <div className="rounded-full bg-slate-400 w-12 h-12"></div>
-      <div className="text-left text-sm">
-        <div className="font-semibold">Club of coders</div>
-        <div>@codeOfCoders.com</div>
-      </div>
-      </div>
-      <div><BsThreeDots/></div>
-    </button>
-  </section>  )
-}
+    </section>
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
