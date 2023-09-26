5b61ad65851cb284d51dfbed6a7d07c7b9d9b1d4
Gurveer Madurai <maduraigurveer@gmail.com>
2023-09-26 10:35:37 -0700

    main component structure defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MainComponent from "@/components/MainComponent";
 
 const page = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-black">
-      <div className="max-w-screen-lg w-full h-full flex relative">
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-xl w-full h-full flex relative">
         {/* Left sidebar/ navigation */}
         <LeftSidebar />
-        <main className="ml-[275px] p-6 mx-2 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-          <h1 className="text-2xl font-bold">Home</h1>
-        </main>
-        <section></section>
+        <MainComponent/>
+        {/* <section></section> */}
       </div>
     </div>
   );
