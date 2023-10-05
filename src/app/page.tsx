30ffc7fc7ed56fa5993d0e10305cea5bc005d8b3
Gurveer Madurai <maduraigurveer@gmail.com>
2023-10-04 19:07:03 -0700

    shadncui packages installed and imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import RightSection from "@/components/RightSection";
 
 const page = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center relative bg-black">
+    <div className="w-full h-full flex justify-center items-center text-white relative bg-black">
       <div className="max-w-[70vw] w-full h-full flex relative">
         {/* Left sidebar/ navigation */}
         <LeftSidebar />
